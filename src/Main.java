203faffaeab49c2f426c0e00a26e4d91034db03a
William Miranda <wlmirand@gmail.com>
2018-09-25 16:07:53 -0300

    Acertando a Main
    Signed-off-by: William Miranda <wlmirand@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -70,22 +70,22 @@ public class Main {
 
     private static void testeKSmall() {
         KSmall.TreeNode root = new KSmall.TreeNode();
-        root.val = 5;
+        root.value = 5;
 
         root.right = new KSmall.TreeNode();
-        root.right.val = 6;
+        root.right.value = 6;
 
         root.left = new KSmall.TreeNode();
-        root.left.val = 3;
+        root.left.value = 3;
 
         root.left.right = new KSmall.TreeNode();
-        root.left.right.val = 4;
+        root.left.right.value = 4;
 
         root.left.left = new KSmall.TreeNode();
-        root.left.left.val = 2;
+        root.left.left.value = 2;
 
         root.left.left.left = new KSmall.TreeNode();
-        root.left.left.left.val = 1;
+        root.left.left.left.value = 1;
 
         KSmall ksm = new KSmall();
 
